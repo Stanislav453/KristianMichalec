a1edf45bb5c4ee3c546f814c192ab4d73fe10438
Stanislav Jendrišák <st.jendrisakgmail.com>
2022-06-24 15:38:06 +0200

    Vytvoril som back-to-home-button

--- js/script.js
+++ js/script.js
@@ -15,6 +15,22 @@ let sluzbyBlockLi = document.querySelectorAll(".sluzby-block-left ul > li");
     })
  }
 
+// == back to home button ===================================
+
+let backToHome = document.querySelector(".back_to_home");
+
+window.addEventListener("scroll", function(e){
+    let scroll = this.window.scrollY;
+
+    if(Math.ceil(scroll) > 400) {
+        backToHome.style.display = "block";
+
+    } else {
+        backToHome.style.display = "none";
+    }
+
+})
+
 
 
 
